52138851bb40e2cf07493dee231ec83ee3c4b1d4
SANTI <ipineros@uniempresarial.edu.co>
2025-12-01 11:27:00 -0500

    cambios preuba

--- backend/api/models/usuario.py
+++ backend/api/models/usuario.py
@@ -1,13 +1,22 @@
-from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-class Usuario(AbstractUser):
-    """
-    Modelo personalizado de usuario.
-    """
-    telefono = models.CharField(max_length=20, blank=True, null=True)
-    direccion = models.CharField(max_length=255, blank=True, null=True)
+class Usuario(models.Model):
+    class TipoUsuario(models.TextChoices):
+        TURISTA = "TURISTA"
+        PRESTADOR = "PRESTADOR"
+        EMPRESA = "EMPRESA"kjgkjgk
 
-    class Meta:
-        verbose_name = "Usuario"
-        verbose_name_plural = "Usuarios"
+    nombre = models.CharField(max_length=100)
+    apellido = models.CharField(max_length=100)
+    fecha_nacimiento = models.DateTimeField()
+    email = models.EmailField(unique=True)
+    password_hash = models.CharField(max_length=250)
+    numero_telefonico = models.CharField(max_length=20)
+    tipo_identificacion = models.CharField(max_length=50)
+    num_identificacion = models.CharField(max_length=50, unique=True)
+    url_foto_documento = models.CharField(max_length=255)
+    nombre_tipo = models.CharField(max_length=30, choices=TipoUsuario.choices)
+    activo = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.email
