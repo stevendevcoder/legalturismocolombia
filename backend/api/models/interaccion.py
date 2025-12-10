4a3cf1639227343472d4f18e63ebc51113237e3c
Andres9034 <quintanillaandres04@gmail.com>
2025-12-10 10:47:24 -0500

    comentario eliminado

--- backend/api/models/interaccion.py
+++ backend/api/models/interaccion.py
@@ -10,7 +10,7 @@ class Interaccion(models.Model):
     usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
     prestador = models.ForeignKey(PrestadorServicio, on_delete=models.CASCADE)
     fecha_interaccion = models.DateTimeField(auto_now_add=True)
-    tipo = models.CharField(max_length=50, default='reserva') # reserva, visita, etc.
+    tipo = models.CharField(max_length=50, default='reserva')
 
     def __str__(self):
         return f"Interacción {self.usuario} - {self.prestador}"
