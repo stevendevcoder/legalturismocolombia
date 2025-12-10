f7a687444c7dffda9bcb63989bcdb17bbeb2d699
Andres9034 <quintanillaandres04@gmail.com>
2025-12-10 11:38:38 -0500

    Eliminado de comentario

--- backend/api/views/usuario_controller.py
+++ backend/api/views/usuario_controller.py
@@ -3,7 +3,7 @@ from rest_framework.response import Response
 from rest_framework import permissions, status
 from ..services.usuario_service import UsuarioService
 
-# 1. Endpoint: GET /api/auth/me/
+
 class MeAPIView(APIView):
     permission_classes = [permissions.IsAuthenticated]
 
@@ -15,10 +15,10 @@ class MeAPIView(APIView):
     def patch(self, request):
         usuario = request.user
         try:
-            # Llamamos al servicio para actualizar (incluyendo datos anidados de Turista/Empresa)
+            
             usuario_actualizado = UsuarioService.actualizar_usuario(usuario, request.data)
             
-            # Devolvemos los datos ya actualizados
+            
             data = UsuarioService.obtener_detalles_completos(usuario_actualizado)
             return Response(data, status=status.HTTP_200_OK)
             
