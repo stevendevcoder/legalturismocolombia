4d0ad28766ccb119e4e62438d6cecb857fcf6784
Andres9034 <quintanillaandres04@gmail.com>
2025-12-10 11:15:20 -0500

    Eliminado de comentario

--- backend/api/serializers/service_serializers.py
+++ backend/api/serializers/service_serializers.py
@@ -3,11 +3,11 @@ from api.models.servicio import CardServicioVenta
 from api.serializers.provider_serializers import EmpresaPrestadoraSerializer, PrestadorIndividualSerializer
 
 class ServiceSerializer(serializers.ModelSerializer):
-    # Optional: Display provider info
+    
     empresa_prestadora_detail = EmpresaPrestadoraSerializer(source='empresa_prestadora', read_only=True)
     prestador_individual_detail = PrestadorIndividualSerializer(source='prestador_individual', read_only=True)
 
-    # Relax validation for this field to allow handling it in the view
+    
     url_imagen_principal = serializers.CharField(required=False, allow_blank=True)
 
     class Meta:
