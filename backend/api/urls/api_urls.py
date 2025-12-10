e285531d1ac86bc0153603c60e16750bcf9ee0e1
Andres9034 <quintanillaandres04@gmail.com>
2025-12-10 11:20:31 -0500

    Eliminado de comentario

--- backend/api/urls/api_urls.py
+++ backend/api/urls/api_urls.py
@@ -30,25 +30,24 @@ router.register(r'services', ServiceViewSet, basename='service')
 router.register(r'employees', EmpleadoViewSet, basename='employee')
 
 urlpatterns = [
-    # --- Autenticación ---
+   
     path('auth/register/', RegisterAPIView.as_view(), name='auth_register'),
     path('auth/logout/', LogoutAPIView.as_view(), name='auth_logout'),
     path('auth/me/', MeAPIView.as_view(), name='auth_me'),
     path('auth/login/', LoginAPIView.as_view(), name='auth_login'),
     path('users/profile/', MeAPIView.as_view(), name='user_profile'),
-    # --- JWT Tokens ---
+    
     path('api/auth/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('prestadores/registro/', RegistroPrestadorView.as_view(), name='registro_prestador'),
     path('servicios/buscar/', BusquedaServiciosView.as_view(), name='buscar_servicios'),
     path('qr/verificar/<str:codigo>/', VerificacionQRView.as_view(), name='verificar_qr'),
     path("prestadores/<int:prestador_id>/qr/", GenerarQRPrestadorView.as_view()),
-    # Reseñas
+    
     path('services/<int:id>/reviews/', ListarCalificacionView.as_view()),
     path('services/<int:id>/reviews/create/', CrearCalificacionView.as_view()),
 
-    # Denuncias
-    # Denuncias
+    
     path('reports/', CrearDenunciaView.as_view()),
     path('reports/my-reports/', MisDenunciasView.as_view()),
 ] + router.urls
